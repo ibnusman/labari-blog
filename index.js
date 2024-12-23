ff69bac5f927c21b7f7c3ba1b00f3d687a964a88
ibnusman <abusaleh3035@gmail.com>
2024-12-23 07:42:57 +0100

    Uodate header file

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+});
+
 //setting the listening port 
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`);
